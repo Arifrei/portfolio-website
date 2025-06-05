84c3f7085afabed68ea2f9593074ca669b221873
Ari <aronf11121@gmail.com>
2025-06-05 11:57:43 -0400

    Update contact functionlity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+import smtplib
+from email.message import EmailMessage
 from dotenv import load_dotenv
 import json
 import os
@@ -26,6 +28,22 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
+        sender_email = os.getenv("EMAIL_ADDRESS")
+        recipient_email = os.getenv("RECIPIENT_EMAIL")
+        app_password = os.getenv("EMAIL_PASSWORD")
+
+        msg = EmailMessage()
+        msg["Subject"] = "Portfolio Website Contact"
+        msg["From"] = sender_email
+        msg["To"] = recipient_email
+        msg.set_content(f"""
+        Name: {data['name']}
+        Email: {data['email']}
+        
+        Message: {data['message']}
+        """)
+        flash("Your message has been sent!")
+        return redirect(url_for('contact.html'))
     return render_template('contact.html')
 
 
