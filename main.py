2b95e71e960a83291fff30e54d77007341da3045
Ari <aronf11121@gmail.com>
2025-06-16 17:02:17 -0400

    Correct redirect url

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def contact():
         Message: {data['message']}
         """)
         flash("Your message has been sent!")
-        return redirect(url_for('contact.html'))
+        return redirect(url_for('contact'))
     return render_template('contact.html')
 
 
