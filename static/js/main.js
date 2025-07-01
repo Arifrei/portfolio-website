2ccb48472ae9a534f38343a0d7fbb96c7496b572
Ari <aronf11121@gmail.com>
2025-07-01 18:41:33 -0400

    Continue updating design and functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 // main.js
 document.addEventListener('DOMContentLoaded', function () {
-    const fadeElements = document.querySelectorAll('.fade-in');
-    fadeElements.forEach(el => {
-        el.style.opacity = 0;
-        setTimeout(() => {
-            el.style.opacity = 1;
-        }, 300);
-    });
+  const fadeElements = document.querySelectorAll('.fade-in');
+  fadeElements.forEach((el, i) => {
+    el.style.opacity = 0;
+    setTimeout(() => {
+      el.style.opacity = 1;
+    }, 100 + i * 100);
+  });
 });
